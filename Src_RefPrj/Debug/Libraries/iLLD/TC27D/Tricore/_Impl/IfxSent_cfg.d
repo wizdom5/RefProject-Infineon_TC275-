1624db989240852a288067b15557664f40a02246
wizdom5 <shine5601@nvaer.com>
2025-03-15 20:50:22 +0900

    gitignore 반영

--- Src_RefPrj/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-c:/Users/ldh/Documents/_workspace/Reference_Project(Infineon_TC275)/Src_RefPrj/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.c
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.c :
-c:/Users/ldh/Documents/_workspace/Reference_Project(Infineon_TC275)/Src_RefPrj/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.h
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.h :
--- Src_RefPrj/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-c:/Users/ldh/Documents/_workspace/Reference_Project(Infineon_TC275)/Src_RefPrj/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.c
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.c :
-c:/Users/ldh/Documents/_workspace/Reference_Project(Infineon_TC275)/Src_RefPrj/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.h
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.h :
